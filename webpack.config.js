8f0e0ca31792ad506470203a0c537e0ef61953d4
Robin Stut <robin@voorhoede.nl>
2020-11-19 14:55:52 +0100

    build(webpack): creating ssg

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    home: ['./src/pages/home/home.js', './src/components/canvas/canvas.js'],
+
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]/script.js'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: '[contenthash].[ext]',
+              outputPath: 'assets',
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        enforce: 'pre'
+      },
+      {
+        test: [/\.js$/, /\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react'],
+          plugins: ['@babel/plugin-transform-runtime'],
+        },
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './home/index.html',
+      template: '!!ejs-webpack-loader!src/pages/home/home.ejs',
+      templateParameters: {
+        'data': require('./.dato/data.json'),
+      },
+      chunks: ['home'],
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    })
+  ]
+};
