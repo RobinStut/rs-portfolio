ad0dee71e4e06d9ecbf299b5fbab2c03bee12ca3
Robin Stut <robin@voorhoede.nl>
2020-11-19 14:54:50 +0100

    fix(dato): dumps data in 1 file

--- dato.config.js
+++ dato.config.js
@@ -1,39 +1,86 @@
 module.exports = (dato, root, i18n) => {
 
-  const getSchema = (key, value) => {
-    const markdown = require( "markdown" ).markdown
-
-    let schema = {}
-
+  const validate = (key, value) => {
+    const markdown = require("markdown").markdown
     switch (key) {
-      case 'index':
-        let { parallax, content } = value
-        parallax = Object.values(parallax).map(x => x.upload.url)
-
-        schema = {
-          title: value.title,
-          parallax: parallax,
-          content: markdown.toHTML(content)
-        }
+      case 'author':
+        value = markdown.toHTML(value)
         break;
-      case 'contact':
-        const { contactDetails, routeDetails, location } = value
-
-        schema = {
-          title: value.title,
-          contact: contactDetails,
-          route: routeDetails,
-          location: location
-        }
+      case 'image':
+        value = value.upload.url
+        break;
+      case 'seoMeta':
+        const metaTags = []
+        Object.entries(value.value).forEach((seoKey, seoVal) => {
+          switch (seoKey) {
+            case 'title':
+              metaTags.push(`<meta property="og:title" content="${seoVal}">`)
+              break;
+            case 'description':
+              metaTags.push(`<meta property="og:description" content="${seoVal}">`)
+              break;
+            case 'image':
+              metaTags.push(`<meta property="og:image" content="${seoVal}">`)
+              break
+            default:
+              break;
+          }
+          value = metaTags
+        })
+        value = ""
         break;
       default:
         break;
     }
-    return schema
+    return value
   }
 
-  for (let [key, value] of Object.entries(dato.collectionsByType)) {
-    root.createDataFile(`src/${key}.json`, "json", getSchema(key, value))
+
+  const stripData = (key, value) => {
+    const obj = Object.assign({}, value);
+
+    // delete unneeded values
+    delete obj.entity;
+    delete obj.itemsRepo;
+
+    return obj
   }
 
+  const convertData = (value) => {
+
+    const obj = {}
+
+    for (let [key, val] of Object.entries(value)) {
+      const validatedData = validate(key, val)
+      obj[key] = validatedData
+    }
+
+    return obj
+  }
+
+  const createData = () => {
+    const schema = {}
+
+    for (let [key, value] of Object.entries(dato.collectionsByType)) {
+      schema[key] = ""
+
+      if (value.length) {
+        const arr = []
+        value.forEach(val => {
+          const strippedData = stripData(key, val)
+          arr.push(convertData(strippedData))
+        })
+        schema[key] = arr
+      }
+      else {
+        const strippedData = stripData(key, value)
+        schema[key] = convertData(strippedData)
+      }
+    }
+
+    root.createDataFile(`.dato/data.json`, "json", schema)
+
+  }
+
+  createData()
 };
